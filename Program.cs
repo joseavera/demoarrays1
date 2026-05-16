502e2d38e31ea4392abe3ddfac3ec7420cffc82b
@joseavera <69088516+joseavera@users.noreply.github.com>
2026-05-16 21:08:00 +0000

    cree un arreglo y accedi a sus elementos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        int[] notas = {81, 92, 78, 96, 61};
+
+        for (int i=0; i < notas.Length; i++)
+        {
+            Console.WriteLine($"Posicion [{i}]: {notas[i]}");
+        }
+
+    }
+
+}
